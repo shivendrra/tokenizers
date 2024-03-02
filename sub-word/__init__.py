4b7df63ea499a9272b86c917e3a18dffc8debbf7
Shivendra <shivharsh44@gmail.com>
2024-03-03 00:09:02 +0530

    made it a module

---
+++ sub-word/__init__.py
@@ -0,0 +1,3 @@
+from .regex import RegexTokenizer
+from .tiktoken import GPT4tokenizer
+from .base import get_stats, merge, encode, decode, train, save_model, replace_control_characters, apply_regex, load
