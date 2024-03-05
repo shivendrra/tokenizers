73bffb33bfe3822e4e22314166475294213755ed
Shivendra <shivharsh44@gmail.com>
2024-03-05 23:51:23 +0530

    it works, it fucking works!!

--- test.py
+++ test.py
@@ -27,41 +27,22 @@ these kinds of scammers are. Some of the most common
 government scams include Covid scams, social security scams, and as you're about to see, IRS scams.
 """
 
-from miniBPE import RegexTokenizer, GPT4tokenizer, save_model
+from miniBPE import BasicTokenizer
 import timeit
-
-llama_text = """
-<|endoftext|>The llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama] or [ˈʝama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the pre-Columbian era.
-Llamas are social animals and live with others as a herd. Their wool is soft and contains only a small amount of lanolin.[2] Llamas can learn simple tasks after a few repetitions. When using a pack, they can carry about 25 to 30% of their body weight for 8 to 13 km (5–8 miles).[3] The name llama (in the past also spelled "lama" or "glama") was adopted by European settlers from native Peruvians.[4]
-The ancestors of llamas are thought to have originated from the Great Plains of North America about 40 million years ago, and subsequently migrated to South America about three million years ago during the Great American Interchange. By the end of the last ice age (10,000–12,000 years ago), camelids were extinct in North America.[3] As of 2007, there were over seven million llamas and alpacas in South America and over 158,000 llamas and 100,000 alpacas, descended from progenitors imported late in the 20th century, in the United States and Canada.[5]
-<|fim_prefix|>In Aymara mythology, llamas are important beings. The Heavenly Llama is said to drink water from the ocean and urinates as it rains.[6] According to Aymara eschatology,<|fim_suffix|> where they come from at the end of time.[6]<|fim_middle|> llamas will return to the water springs and ponds<|endofprompt|>
-""".strip()
-
-specials_string = """
-<|endoftext|>Hello world this is one document
-<|endoftext|>And this is another document
-<|endoftext|><|fim_prefix|>And this one has<|fim_suffix|> tokens.<|fim_middle|> FIM
-<|endoftext|>Last document!!! 👋<|endofprompt|>
-""".strip()
-
-special_tokens = {
-  '<|endoftext|>': 100257,
-  '<|fim_prefix|>': 100258,
-  '<|fim_middle|>': 100259,
-  '<|fim_suffix|>': 100260,
-  '<|endofprompt|>': 100276
-}
 os.makedirs('../models', exist_ok=True)
-time01 = timeit.default_timer()
 
-name = '../models/regex'
-tokenizer = GPT4tokenizer()
-# tokenizer.train(text, 512, verbose=True)
-# save_model(name)
+with open('../captions.txt', 'r', encoding='utf-8') as f:
+  train_text = f.read()
+
+time01 = timeit.default_timer()
+name = '../models/basicCharMap'
+
+tokenizer = BasicTokenizer(train_text)
+tokenizer.load_model('../models/basicCharMap.model')
+# tokenizer.train(n_merges=512)
+# tokenizer.save_model(name)
 
 time02 = timeit.default_timer()
 
-test_str = "Hello, what's your name?"
-print(f'total time taken: {(time02 - time01)} secs')
-print(tokenizer.encode(test_str))
-print(llama_text==tokenizer.decode(tokenizer.encode(llama_text)))
+print(f'total time taken: {(time02 - time01)/60} mins')
+print(text==tokenizer.decode(tokenizer.encode(text)))
