5a4c179cf592042a3483176f2746147bb55ce294
Shivendra <shivharsh44@gmail.com>
2024-03-05 23:18:25 +0530

    fixed all the issues

--- miniBPE/__init__.py
+++ miniBPE/__init__.py
@@ -1,3 +1,4 @@
 from .regex import RegexTokenizer
 from .tiktoken import GPT4tokenizer
 from .base import get_stats, merge, encode, decode, train, save_model, replace_control_characters, apply_regex, load
+from .charMap import BasicTokenizer
--- miniBPE/charMap.py
+++ miniBPE/charMap.py
@@ -14,21 +14,20 @@ class BasicTokenizer:
   def __init__(self, train_text):
     super().__init__()
     self.chars = sorted(list(set(train_text)))
+    self.train_data = train_text
     self.vocab_size = len(self.chars)
     self.string_to_index = { ch:i for i,ch in enumerate(self.chars) }
     self.index_to_string = { i:ch for i,ch in enumerate(self.chars) }
 
-  def encode(self, string):
-    for char in string:
-      s = self.string_to_index(char)
-      return s
+  def _encode(self, string):
+    encoded = [self.string_to_index[char] for char in string]
+    return encoded
   
-  def decode(self, integer):
-    for no in integer:
-      s = self.index_to_string(no)
-      return s
-  
-  def get_stats(self, ids, counts=None):
+  def _decode(self, integer):
+    decoded = ''.join([self.index_to_string[i] for i in integer])
+    return decoded
+
+  def _get_stats(self, ids, counts=None):
     """
       takes list of integers and returns dictionary of counts of pairs(consecutive ones)
       eg: [1, 2, 3, 1, 2] -> {(1, 2): 2, (2, 3): 1, (3, 1): 1}
@@ -39,7 +38,7 @@ class BasicTokenizer:
       counts[pair] = counts.get(pair, 0) + 1
     return counts
   
-  def merge(self, ids, pair, idx):
+  def _merge(self, ids, pair, idx):
     """
       in the list of integers, replaces all consecutive pair with the new integer token idx
       eg: ids=[1, 2, 3, 1, 2], pair=(1, 2), idx=4 -> [4, 3, 4]
@@ -55,5 +54,84 @@ class BasicTokenizer:
         i += 1
     return new_ids
   
-  def train_model(self, train_data, n_merges):
-    pass
+  def train(self, n_merges):
+    tokens = list(self._encode(self.train_data))
+    ids = list(tokens)
+
+    merges = {}
+    vocab = {i: ids for i, ids in enumerate(self.chars)}
+    for i in tqdm(range(n_merges), desc='Training the tokenizer\t'):
+      stats = self._get_stats(ids)
+      pair = max(stats, key=stats.get)
+      idx = self.vocab_size + i
+      ids = self._merge(ids, pair, idx)
+      merges[pair] = idx
+    
+    for (p0, p1), idx in merges.items():
+      vocab[idx] = vocab[p0] + vocab[p1]
+    
+    self.vocab = vocab
+    self.merges = merges
+    return self.vocab, self.merges
+
+  def _build_vocab(self, merges):
+    vocab = {i: ids for i, ids in enumerate(self.chars)}
+    for (p0, p1), idx in merges.items():
+      vocab[idx] = vocab[p0] + vocab[p1]
+
+    return vocab
+  
+  def encode(self, en_text):
+    tokens = list(self._encode(en_text))
+    while True:
+      stats = self._get_stats(tokens)
+      pair = min(stats, key=lambda p: self.merges.get(p, float('inf')))
+      if pair not in self.merges:
+        break
+      idx = self.merges[pair]
+      tokens = self._merge(tokens, pair, idx)
+    return tokens
+  
+  def decode(self, de_text):
+    tokens = [self.vocab[idx] for idx in de_text]
+    text = ''.join(tokens)
+    return text
+
+  def save_model(self, prefix):
+    """
+      Saves two files: file_prefix.vocab and file_prefix.model
+      This is inspired (but not equivalent to!) sentencepiece's model saving:
+      - model file is the critical one, intended for load()
+      - vocab file is just a pretty printed version for human inspection only
+      ## not suitable to be called, has some issues
+    """
+    file_name = prefix + '.model'
+    inverted_merges = {idx: pair for pair, idx in self.merges.items()}
+    with open(file_name, 'w', encoding='utf-8') as f:
+      f.write("minibpe v1\n")
+      for idx1, idx2 in tqdm(self.merges, desc='Saving model\t\t', unit='merge'):
+        f.write(f"{idx1} {idx2}\n")
+
+    print(f'file saved successfully!')
+
+  def load_model(self, model_file):
+    """
+      loads '.model' file
+      returns vocab loaded from the file
+      ## not suitable to be called, has some issues
+    """
+    assert model_file.endswith('.model')
+    merges = {}
+    idx = self.vocab_size
+    with open(model_file, 'r', encoding='utf-8') as f:
+      version = f.readline().strip()
+      assert version == "minibpe v1"
+      for line in tqdm(f, desc='Loading model\t\t'):
+        idx1, idx2 = map(int, line.split())
+        merges[(idx1, idx2)] = idx
+        idx += 1
+      
+      self.merges = merges
+      self.vocab = self._build_vocab(self.merges)
+    
+    return self.vocab, self.merges
